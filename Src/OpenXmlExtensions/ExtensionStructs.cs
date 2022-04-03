a877bb19f4ef06cc3d5b405151ea4508cde3aa8d
Shayan Toqraee <Shayan.To@Hotmail.com>
2022-04-04 00:16:26 +0430

    Use short syntax for structs.

--- Src/OpenXmlExtensions/ExtensionStructs.cs
+++ Src/OpenXmlExtensions/ExtensionStructs.cs
@@ -1,61 +1,18 @@
 namespace RevayatUniqueFinder;
 
-public record struct ExtensionElement<TElement> where TElement : OpenXmlElement
+public readonly record struct ExtensionElement<TElement>(TElement Element) where TElement : OpenXmlElement
 {
-    public ExtensionElement(TElement element)
-    {
-        this.Element = element;
-    }
-
     public static implicit operator ExtensionElement<OpenXmlElement>(ExtensionElement<TElement> element)
     {
         return new(element.Element);
     }
-
-    public TElement Element { get; }
 }
 
-public record struct ExtensionValue<T>
-{
-    public ExtensionValue(T value, ExtensionValueSource source)
-    {
-        this.Value = value;
-        this.Source = source;
-    }
+public readonly record struct ExtensionValue<T>(T Value, ExtensionValueSource Source);
 
-    public T Value { get; }
-    public ExtensionValueSource Source { get; }
-}
+public readonly record struct ExtensionValueSource(ExtensionElement<OpenXmlElement> Element, ExtensionValueSourceType Type, XmlQualifiedName? Attribute = null);
 
-public record struct ExtensionValueSource
-{
-    public ExtensionValueSource(ExtensionElement<OpenXmlElement> element, ExtensionValueSourceType type, XmlQualifiedName? attribute = null)
-    {
-        this.Element = element;
-        this.Type = type;
-        this.Attribute = attribute;
-    }
-
-    public ExtensionElement<OpenXmlElement> Element { get; }
-    public ExtensionValueSourceType Type { get; }
-    public XmlQualifiedName? Attribute { get; }
-}
-
-public record struct XmlQualifiedName
-{
-    public XmlQualifiedName(string propertyName, string name, string namespaceUri, string namespacePrefix)
-    {
-        this.PropertyName = propertyName;
-        this.Name = name;
-        this.NamespaceUri = namespaceUri;
-        this.NamespacePrefix = namespacePrefix;
-    }
-
-    public string PropertyName { get; }
-    public string Name { get; }
-    public string NamespaceUri { get; }
-    public string NamespacePrefix { get; }
-}
+public readonly record struct XmlQualifiedName(string PropertyName, string Name, string NamespaceUri, string NamespacePrefix);
 
 public enum ExtensionValueSourceType
 {
